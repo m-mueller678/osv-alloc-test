12a34d2e17607ec2cbab0dad74490cea13f970f6
m-mueller678 <marcus.mueller.678@gmail.com>
2024-02-07 18:27:20 +0100

    robinhood wip

--- src/PageMap.rs
+++ src/PageMap.rs
@@ -1,117 +1,178 @@
+use ahash::AHasher;
+use modular_bitfield::prelude::*;
 use std::sync::atomic::AtomicU64;
 use std::sync::atomic::Ordering::Relaxed;
 use std::thread::yield_now;
-use ahash::AHasher;
-use modular_bitfield::prelude::*;
-use x86_64::structures::paging::{Page, Size2MiB, PhysFrame, PageSize};
+use x86_64::structures::paging::{Page, PageSize, PhysFrame, Size2MiB};
 
-const FRAME_BITS:u32=19;
-const COUNT_BITS:u32=16;
-const PAGE_BITS:u32=63-COUNT_BITS-FRAME_BITS;
+const FRAME_BITS: u32 = 19;
+const COUNT_BITS: u32 = 16;
+const PAGE_BITS: u32 = 63 - COUNT_BITS - FRAME_BITS;
 
-const PAGE_SHIFT:u32=0;
-const FRAME_SHIFT:u32=PAGE_BITS;
-const COUNT_SHIFT:u32=FRAME_SHIFT+FRAME_BITS;
+const PAGE_SHIFT: u32 = 0;
+const FRAME_SHIFT: u32 = PAGE_BITS;
+const COUNT_SHIFT: u32 = FRAME_SHIFT + FRAME_BITS;
 
-type PageField=B28;
-type FrameField=B19;
-type CountField=B16;
+type PageField = B28;
+type FrameField = B19;
+type CountField = B16;
 
-#[bitfield(bits=64)]
+#[bitfield(bits = 64)]
 #[repr(align(8))]
-#[derive(Copy)]
-struct PageRecord{
-    page:PageField,
-    frame:FrameField,
-    count:CountField,
-    locked:bool,
+#[derive(Clone,Copy)]
+struct PageRecord {
+    page: PageField,
+    frame: FrameField,
+    count: CountField,
+    is_locked: bool,
 }
 
-impl PageRecord{
-    fn locked(mut self)->Self{
-        self.set_locked(true);
+impl PageRecord {
+    fn lock(mut self) -> Self {
+        self.set_is_locked(true);
         self
     }
 
-    fn to_u64(self)->u64{
+    fn to_u64(self) -> u64 {
         u64::from_ne_bytes(self.into_bytes())
     }
 
-    fn from_u64(x:u64)->Self{
+    fn from_u64(x: u64) -> Self {
         Self::from_bytes(x.to_ne_bytes())
     }
 }
 
-struct PageMap{
-    base_page:Page<Size2MiB()>,
-    slot_index_mask:usize,
-    slots:Vec<AtomicU64>,
-    random_state:ahash::RandomState,
+struct PageMap {
+    base_page: Page<Size2MiB>,
+    slot_index_mask: usize,
+    slots: Vec<AtomicU64>,
+    random_state: ahash::RandomState,
 }
 
-const MAX_ALLOCS_PER_PAGE:usize = 1<<COUNT_BITS-1;
+const MAX_ALLOCS_PER_PAGE: usize = 1 << COUNT_BITS - 1;
 
-impl PageMap{
-    pub fn decrement_and_remove_0(&self,page:Page::<Size2MiB>)->Option<PhysFrame<Size2MiB>>{
-        unimplemented!()
-    }
+impl PageMap {
+    pub fn decrement_and_remove_0(&self, page: Page<Size2MiB>) -> Option<PhysFrame<Size2MiB>> {
+        let target_page=PageField::try_from(page - self.base_page).unwrap();
+        let mut target_slot = self.target_slot(target_page);
+        let first_target_slot = target_slot;
+        let mut scan_slot = target_slot;
+        loop {
+            let Ok(update_result):Result<_,()> = self.update(target_slot, |p| {
+                if p.page() ==target_page && p.count()>0 {
+                    let new_count = p.count()-1;
 
-    pub fn insert(&self,page:Page::<Size2MiB>,frame:PhysFrame<Size2MiB>,count:usize)->Option<PhysFrame<Size2MiB>>{
-        let page_index = page - self.base_page;
-        let frame_index = frame.start_address().as_u64() / Size2MiB::SIZE;
-        debug_assert!(page_index < 1<<PAGE_BITS);
-        debug_assert!(frame_index < 1<<FRAME_BITS);
-        debug_assert!(count<1<<COUNT_BITS);
-        let mut to_insert = page_index | ((count as u64) << FRAME_BITS | frame_index)<<PAGE_BITS;
-        let mut target_slot = self.target_slot(record);
-        let mut scan_slot=target_slot;
-        loop{
-            let update_result = self.update(target_slot,|p|{
-                if p.count()==0{
-                    Ok((to_insert,None))
-                }else{
+                } else {
                     let other_target_slot = self.target_slot(p);
-                    if self.psl(target_slot,scan_slot) < self.psl(other_target_slot,scan_slot){
-                        Ok((p.locked(),Some((target_slot,to_insert))))
-                    }else{
-                        Ok((to_insert.locked(),(other_target_slot,p)))
+                    if self.psl(target_slot, scan_slot) < self.psl(other_target_slot, scan_slot) {
+                        Ok((p.lock(), Some((target_slot, to_insert))))
+                    } else {
+                        Ok((to_insert.lock(), Some((other_target_slot, p))))
                     }
                 }
-            });
-            let present = PageRecord::load(&self.slots[target_slot]);
-            if present.count() == 0{
-
+            }) else {
+                unreachable!()
+            };
+            if let Some((new_target, new_record)) = update_result {
+                target_slot = new_target;
+                to_insert = new_record;
+                scan_slot += 1;
             }else{
-                debug_assert!(present.page()!=page_index);
+                break;
             }
+        }
+        for i in first_target_slot..scan_slot{
+            self.unlock(i);
+        }
+
+    }
+
+    pub fn insert(
+        &self,
+        page: Page<Size2MiB>,
+        frame: PhysFrame<Size2MiB>,
+        count: usize,
+    ){
+        let mut to_insert = PageRecord::new();
+        to_insert.set_count(count.try_into().unwrap());
+        to_insert.set_frame(
+            (frame.start_address().as_u64() / Size2MiB::SIZE)
+                .try_into()
+                .unwrap(),
+        );
+        to_insert.set_page((page - self.base_page).try_into().unwrap());
+        let mut target_slot = self.target_slot(to_insert);
+        let first_target_slot = target_slot;
+        let mut scan_slot = target_slot;
+        loop {
+            let Ok(update_result):Result<_,()> = self.update(target_slot, |p| {
+                if p.count() == 0 {
+                    Ok((to_insert, None))
+                } else {
+                    let other_target_slot = self.target_slot(p);
+                    if self.psl(target_slot, scan_slot) < self.psl(other_target_slot, scan_slot) {
+                        Ok((p.lock(), Some((target_slot, to_insert))))
+                    } else {
+                        Ok((to_insert.lock(), Some((other_target_slot, p))))
+                    }
+                }
+            }) else {
+                unreachable!()
+            };
+            if let Some((new_target, new_record)) = update_result {
+                target_slot = new_target;
+                to_insert = new_record;
+                scan_slot += 1;
+            }else{
+                break;
+            }
+        }
+        for i in first_target_slot..scan_slot{
+            self.unlock(i);
         }
     }
 
-    fn target_slot(&self,record:PageRecord)->usize{
-        self.random_state.hash_one(record.page()) as usize & self.slot_index_mask;
+    fn target_slot(&self, page: PageField) -> usize {
+        self.random_state.hash_one(page) as usize & self.slot_index_mask
     }
 
-    fn load(&self,i:usize)->PageRecord{
+    fn load(&self, i: usize) -> PageRecord {
         PageRecord::from_u64(self.slots[i].load(Relaxed))
     }
 
-    fn update<F:FnMut(PageRecord)->Result<(PageRecord,A),B>,A,B>(&self,i:usize,mut f:F,old:PageRecord,new:PageRecord)->Result<A,B>{
-        loop{
-            let mut curr=PageRecord::from_u64(self.slots[i].load(Relaxed));
-            while! curr.locked(){
-                match f(curr){
-                    Ok((n,a))=>match self.slots[i].compare_exchange_weak(curr.to_u64(),n.to_u64(),Relaxed,Relaxed){
-                        Ok(_)=>return Ok(a),
-                        Err(found)=>{ curr=PageRecord::from_u64(found); }
-                    }
-                    Err(b)=>Err(b),
+    fn unlock(&self,i:usize){
+        debug_assert!(self.load(i).is_locked());
+        self.slots[i].fetch_sub(1<<63,Relaxed);
+    }
+
+    fn update<F: FnMut(PageRecord) -> Result<(PageRecord, A), B>, A, B>(
+        &self,
+        i: usize,
+        mut f: F,
+    ) -> Result<A, B> {
+        loop {
+            let mut curr = PageRecord::from_u64(self.slots[i].load(Relaxed));
+            while !curr.is_locked() {
+                match f(curr) {
+                    Ok((n, a)) => match self.slots[i].compare_exchange_weak(
+                        curr.to_u64(),
+                        n.to_u64(),
+                        Relaxed,
+                        Relaxed,
+                    ) {
+                        Ok(_) => return Ok(a),
+                        Err(found) => {
+                            curr = PageRecord::from_u64(found);
+                        }
+                    },
+                    Err(b) => return Err(b),
                 }
             }
             yield_now();
         }
     }
 
-    fn psl(&self,target_slot:usize,actual_slot:usize)->usize{
-        (actual_slot-target_slot) & self.slot_index_mask
+    fn psl(&self, target_slot: usize, actual_slot: usize) -> usize {
+        (actual_slot - target_slot) & self.slot_index_mask
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,24 @@
+use ahash::RandomState;
+use libc::*;
+use rand::distributions::Uniform;
+use rand::prelude::*;
+use rand::rngs::SmallRng;
 use std::alloc::{GlobalAlloc, Layout};
 use std::collections::hash_map::Entry;
 use std::collections::{BTreeMap, HashMap, VecDeque};
-use std::mem::{align_of, MaybeUninit, replace, size_of};
+use std::mem::{align_of, replace, size_of, MaybeUninit};
 use std::ops::Add;
 use std::ptr;
 use std::time::Instant;
-use ahash::RandomState;
-use libc::*;
-use rand::rngs::SmallRng;
-use rand::prelude::*;
-use rand::distributions::Uniform;
-use x86_64::{PhysAddr, VirtAddr};
 use x86_64::registers::control::Cr3;
 use x86_64::structures::idt::ExceptionVector::Virtualization;
-use x86_64::structures::paging::{FrameAllocator, Mapper, OffsetPageTable, Page, PageSize, PageTable, PageTableFlags, PhysFrame, Size2MiB, Size4KiB};
 use x86_64::structures::paging::mapper::UnmapError;
 use x86_64::structures::paging::page::PageRange;
+use x86_64::structures::paging::{
+    FrameAllocator, Mapper, OffsetPageTable, Page, PageSize, PageTable, PageTableFlags, PhysFrame,
+    Size2MiB, Size4KiB,
+};
+use x86_64::{PhysAddr, VirtAddr};
 
 mod PageMap;
 
@@ -30,7 +33,14 @@ fn alloc_mmap<P: PageSize>(count: usize, zeroed: bool) -> PageRange<P> {
     };
     let init_flags = if zeroed { 0 } else { MAP_UNINITIALIZED };
     let p = unsafe {
-        mmap(ptr::null_mut(), count * P::SIZE as usize, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | page_size_flags | init_flags, -1, 0) as *mut u8
+        mmap(
+            ptr::null_mut(),
+            count * P::SIZE as usize,
+            PROT_READ | PROT_WRITE,
+            MAP_PRIVATE | MAP_ANONYMOUS | page_size_flags | init_flags,
+            -1,
+            0,
+        ) as *mut u8
     };
     assert!(!p.is_null());
     let p = Page::<P>::from_start_address(VirtAddr::from_ptr(p)).unwrap();
@@ -67,7 +77,10 @@ impl MmapFrameAllocator {
 }
 
 unsafe fn page_table<'a>() -> OffsetPageTable<'a> {
-    OffsetPageTable::new(&mut *phys_to_virt(Cr3::read().0.start_address()).as_mut_ptr::<PageTable>(), VirtAddr::new(PHYS_OFFSET))
+    OffsetPageTable::new(
+        &mut *phys_to_virt(Cr3::read().0.start_address()).as_mut_ptr::<PageTable>(),
+        VirtAddr::new(PHYS_OFFSET),
+    )
 }
 
 unsafe impl FrameAllocator<Size4KiB> for MmapFrameAllocator {
@@ -76,10 +89,14 @@ unsafe impl FrameAllocator<Size4KiB> for MmapFrameAllocator {
     }
 }
 
-fn claim_frames<P: PageSize>(count: usize) -> impl Iterator<Item=PhysFrame<P>>
-    where for<'a> OffsetPageTable<'a>: Mapper<P> {
+fn claim_frames<P: PageSize>(count: usize) -> impl Iterator<Item = PhysFrame<P>>
+where
+    for<'a> OffsetPageTable<'a>: Mapper<P>,
+{
     alloc_mmap::<P>(count, false).into_iter().map(|page| {
-        unsafe { page.start_address().as_mut_ptr::<u8>().write(0); }
+        unsafe {
+            page.start_address().as_mut_ptr::<u8>().write(0);
+        }
         unsafe { page_table() }.translate_page(page).unwrap()
     })
 }
@@ -92,18 +109,18 @@ struct MappedPageInfo {
 struct PagingAllocator {
     frame_allocator: MmapFrameAllocator,
     available_frames: Vec<PhysFrame<Size2MiB>>,
-    mapped_pages: HashMap<VirtAddr, MappedPageInfo,ahash::RandomState>,
+    mapped_pages: HashMap<VirtAddr, MappedPageInfo, ahash::RandomState>,
     bump: VirtAddr,
     current_page: MappedPageInfo,
 }
 
-impl PagingAllocator{
-    fn new()->Self{
+impl PagingAllocator {
+    fn new() -> Self {
         assert_eq!(PHYS_SIZE % HUGE_PAGE_SIZE, 0);
         assert_eq!(VIRT_SIZE % HUGE_PAGE_SIZE, 0);
         let phys_pages = alloc_mmap::<Size2MiB>(PHYS_SIZE / HUGE_PAGE_SIZE, false);
         for p in phys_pages {
-            unsafe{
+            unsafe {
                 p.start_address().as_mut_ptr::<u8>().write(0);
             }
         }
@@ -112,26 +129,34 @@ impl PagingAllocator{
         println!("mmap done");
         println!("unmapping virtual range pages");
         {
-            let mut pt= unsafe{ page_table() };
+            let mut pt = unsafe { page_table() };
             for p in virt_pages {
-                match unsafe{page_table()}.unmap(p) {
+                match unsafe { page_table() }.unmap(p) {
                     Ok((f, flush)) => {
                         println!("unmapped {f:?} from virtual range");
                         flush.flush();
                     }
-                    Err(UnmapError::PageNotMapped) => { continue; }
+                    Err(UnmapError::PageNotMapped) => {
+                        continue;
+                    }
                     Err(e) => panic!("cannot unmap {p:?} in virtual range: {e:?}"),
                 }
             }
         }
 
         println!("unmapping complete");
-        let ret=PagingAllocator {
+        let ret = PagingAllocator {
             frame_allocator: Default::default(),
-            available_frames: phys_pages.into_iter().map(|p|  unsafe{page_table()}.translate_page(p).unwrap()).collect(),
+            available_frames: phys_pages
+                .into_iter()
+                .map(|p| unsafe { page_table() }.translate_page(p).unwrap())
+                .collect(),
             mapped_pages: HashMap::with_hasher(ahash::RandomState::with_seed(0xee61096f95490820)),
             bump: (virt_pages.last().unwrap() + 1).start_address(),
-            current_page: MappedPageInfo { allocation_count: 0, frame: PhysFrame::containing_address(PhysAddr::new(0)) },
+            current_page: MappedPageInfo {
+                allocation_count: 0,
+                frame: PhysFrame::containing_address(PhysAddr::new(0)),
+            },
         };
         println!("allocator constructed");
         ret
@@ -145,7 +170,7 @@ unsafe trait TestAlloc {
 
 struct OsvAlloc;
 
-unsafe impl TestAlloc for OsvAlloc{
+unsafe impl TestAlloc for OsvAlloc {
     unsafe fn alloc(&mut self, layout: Layout) -> *mut u8 {
         malloc(layout.size()) as *mut u8
     }
@@ -178,12 +203,28 @@ unsafe impl TestAlloc for PagingAllocator {
             for pi in (1..=old_page - min_page).rev() {
                 let store_page = min_page + pi;
                 //println!("storing page {store_page:?} -> {:?}",self.current_page.frame);
-                let was_none = self.mapped_pages.insert(store_page.start_address(), replace(&mut self.current_page, MappedPageInfo {
-                    frame: self.available_frames.pop().unwrap(),
-                    allocation_count: 1,
-                })).is_none();
+                let was_none = self
+                    .mapped_pages
+                    .insert(
+                        store_page.start_address(),
+                        replace(
+                            &mut self.current_page,
+                            MappedPageInfo {
+                                frame: self.available_frames.pop().unwrap(),
+                                allocation_count: 1,
+                            },
+                        ),
+                    )
+                    .is_none();
                 //println!("mapping page {:?} -> {:?}",store_page-1,self.current_page.frame);
-                pt.map_to(store_page - 1, self.current_page.frame, PageTableFlags::PRESENT | PageTableFlags::WRITABLE | PageTableFlags::NO_EXECUTE, &mut self.frame_allocator).unwrap().ignore();
+                pt.map_to(
+                    store_page - 1,
+                    self.current_page.frame,
+                    PageTableFlags::PRESENT | PageTableFlags::WRITABLE | PageTableFlags::NO_EXECUTE,
+                    &mut self.frame_allocator,
+                )
+                .unwrap()
+                .ignore();
                 debug_assert!(was_none);
             }
         }
@@ -194,7 +235,8 @@ unsafe impl TestAlloc for PagingAllocator {
     unsafe fn dealloc(&mut self, ptr: *mut u8, layout: Layout) {
         let start_addr = VirtAddr::from_ptr(ptr);
         let mut min_page = Page::<Size2MiB>::containing_address(start_addr);
-        let max_page = Page::<Size2MiB>::containing_address(start_addr + layout.size() as u64 - 1u64);
+        let max_page =
+            Page::<Size2MiB>::containing_address(start_addr + layout.size() as u64 - 1u64);
         let current_page = Page::<Size2MiB>::containing_address(self.bump);
         if min_page == current_page {
             debug_assert!(self.current_page.allocation_count >= 1);
@@ -225,7 +267,11 @@ fn pin() {
         let mut cpu_set = MaybeUninit::<libc::cpu_set_t>::zeroed().assume_init();
         libc::CPU_ZERO(&mut cpu_set);
         libc::CPU_SET(0, &mut cpu_set);
-        let s = libc::pthread_setaffinity_np(libc::pthread_self(), std::mem::size_of::<libc::cpu_set_t>(), &cpu_set);
+        let s = libc::pthread_setaffinity_np(
+            libc::pthread_self(),
+            std::mem::size_of::<libc::cpu_set_t>(),
+            &cpu_set,
+        );
         assert_eq!(s, 0);
     }
 }
@@ -234,23 +280,31 @@ fn main() {
     pin();
     unsafe {
         println!("ours:");
-        test_alloc::<true,false>(10_000_000, 64*KB, 2*GB, &mut PagingAllocator::new());
+        test_alloc::<true, false>(10_000_000, 64 * KB, 2 * GB, &mut PagingAllocator::new());
         println!("libc malloc:");
-        test_alloc::<true,false>(10_000_000, 64*KB, 2*GB, &mut OsvAlloc);
+        test_alloc::<true, false>(10_000_000, 64 * KB, 2 * GB, &mut OsvAlloc);
     }
 }
 
-fn test_alloc<const VALIDATE: bool,const VALIDATE_FULL:bool>(total_allocs: usize, avg_alloc_size: usize, max_concurrent_size: usize, a: &mut impl TestAlloc) {
+fn test_alloc<const VALIDATE: bool, const VALIDATE_FULL: bool>(
+    total_allocs: usize,
+    avg_alloc_size: usize,
+    max_concurrent_size: usize,
+    a: &mut impl TestAlloc,
+) {
     let start = Instant::now();
 
     assert_eq!(avg_alloc_size % 8, 0);
-    let concurrent_allocs =max_concurrent_size/(avg_alloc_size+avg_alloc_size/4);
+    let concurrent_allocs = max_concurrent_size / (avg_alloc_size + avg_alloc_size / 4);
     let mut rng = SmallRng::seed_from_u64(42);
     let mut allocs = VecDeque::with_capacity(concurrent_allocs);
     let avg_alloc_size = avg_alloc_size / 8;
-    let size_range = Uniform::new(avg_alloc_size - avg_alloc_size / 4, avg_alloc_size + avg_alloc_size / 4);
-    fn layout(l:usize)->Layout{
-        Layout::from_size_align(l*size_of::<usize>(),align_of::<usize>()).unwrap()
+    let size_range = Uniform::new(
+        avg_alloc_size - avg_alloc_size / 4,
+        avg_alloc_size + avg_alloc_size / 4,
+    );
+    fn layout(l: usize) -> Layout {
+        Layout::from_size_align(l * size_of::<usize>(), align_of::<usize>()).unwrap()
     }
     unsafe {
         for i in 0..total_allocs {
@@ -259,9 +313,9 @@ fn test_alloc<const VALIDATE: bool,const VALIDATE_FULL:bool>(total_allocs: usize
                     let x: &mut [MaybeUninit<usize>] = allocs.pop_front().unwrap();
                     let old_id = i - concurrent_allocs;
                     if VALIDATE {
-                        assert!(
-                            if VALIDATE_FULL{&x[..]}else{&x[..1]}.iter().all(|x| x.assume_init_read() == old_id)
-                        )
+                        assert!(if VALIDATE_FULL { &x[..] } else { &x[..1] }
+                            .iter()
+                            .all(|x| x.assume_init_read() == old_id))
                     }
                     (x.as_ptr() as *mut u8, x.len())
                 };
@@ -270,9 +324,13 @@ fn test_alloc<const VALIDATE: bool,const VALIDATE_FULL:bool>(total_allocs: usize
             }
             let len = size_range.sample(&mut rng);
             let ptr = a.alloc(layout(len));
-            let slice = std::slice::from_raw_parts_mut(ptr as *mut MaybeUninit<usize>,len);
-            if VALIDATE{
-                for x in if VALIDATE_FULL{&mut slice[..]}else{&mut slice[..1]}{
+            let slice = std::slice::from_raw_parts_mut(ptr as *mut MaybeUninit<usize>, len);
+            if VALIDATE {
+                for x in if VALIDATE_FULL {
+                    &mut slice[..]
+                } else {
+                    &mut slice[..1]
+                } {
                     x.write(i);
                 }
             }
@@ -281,6 +339,9 @@ fn test_alloc<const VALIDATE: bool,const VALIDATE_FULL:bool>(total_allocs: usize
     }
 
     let end = Instant::now();
-    let duration = end-start;
-    println!("complete. {:.3e} alloc/s", total_allocs as f64 / duration.as_secs_f64());
+    let duration = end - start;
+    println!(
+        "complete. {:.3e} alloc/s",
+        total_allocs as f64 / duration.as_secs_f64()
+    );
 }
