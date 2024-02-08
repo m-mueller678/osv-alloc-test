444fc3c717c2a884ddf549423fb79a34319883e4
m-mueller678 <marcus.mueller.678@gmail.com>
2024-02-08 17:08:39 +0100

    wip

--- src/page_map.rs
+++ src/page_map.rs
@@ -1,3 +1,4 @@
+use std::collections::BTreeMap;
 use ahash::RandomState;
 use static_assertions::const_assert;
 use std::sync::atomic::AtomicU64;
@@ -20,8 +21,8 @@ pub struct PageMap {
     slot_index_mask: usize,
     slots: Vec<AtomicU64>,
     random_state: ahash::RandomState,
-    #[cfg(debug_assertions)]
-    lock: Mutex<()>,
+    #[cfg(feature = "page_map_debug")]
+    lock: Mutex<BTreeMap<Page<Size2MiB>, (PhysFrame<Size2MiB>, u64)>>,
 }
 
 pub const MAX_ALLOCS_PER_PAGE: usize = 1 << (COUNT_BITS - 1);
@@ -42,29 +43,41 @@ impl PageMap {
             slot_index_mask: s - 1,
             slots: (0..s).map(|_| AtomicU64::new(0)).collect(),
             random_state: RandomState::with_seed(0xee61096f95490820),
-            #[cfg(debug_assertions)]
-            lock: Mutex::new(()),
+            #[cfg(feature = "page_map_debug")]
+            lock: Default::default(),
         }
     }
 
     pub fn decrement(&self, page: Page<Size2MiB>) -> Option<PhysFrame<Size2MiB>> {
-        #[cfg(debug_assertions)]
-        let _g = self.lock.lock().unwrap();
-
         const_assert!(PAGE_SHIFT == 0);
         const_assert!(COUNT_SHIFT + COUNT_BITS == 64);
-        let page = page - self.base_page;
-        let mut i = self.target_slot(page);
+        #[cfg(feature = "page_map_debug")] let mut lock = self.lock.lock().unwrap();
+        #[cfg(feature = "page_map_debug")] let debug = &mut lock.get_mut(&page).unwrap();
+        let page_i = page - self.base_page;
+        let mut i = self.target_slot(page_i);
         loop {
             let found = self.slots[i].load(Relaxed);
-            if (found >> COUNT_SHIFT) != 0 && (found & mask(PAGE_BITS)) == page {
+            if (found >> COUNT_SHIFT) != 0 && (found & mask(PAGE_BITS)) == page_i {
                 let old_val = self.slots[i].fetch_sub(1 << COUNT_SHIFT, Relaxed);
-                return if old_val >> COUNT_SHIFT == 1 {
+                let old_count = old_val >> COUNT_SHIFT;
+                #[cfg(feature = "page_map_debug")]
+                assert_eq!(old_count, debug.1);
+                let ret = if old_count == 1 {
                     let frame = (old_val >> FRAME_SHIFT) & mask(FRAME_BITS);
-                    Some(PhysFrame::from_start_address(PhysAddr::new(frame << 21)).unwrap())
+                    let frame = PhysFrame::from_start_address(PhysAddr::new(frame << 21)).unwrap();
+                    #[cfg(feature = "page_map_debug")]
+                    assert_eq!(frame, debug.0);
+                    Some(frame)
                 } else {
                     None
                 };
+                #[cfg(feature = "page_map_debug")]{
+                    debug.1 -= 1;
+                    if debug.1 == 0 {
+                        lock.remove(&page);
+                    }
+                }
+                return ret;
             } else {
                 i = (i + 1) & self.slot_index_mask;
             }
@@ -72,8 +85,11 @@ impl PageMap {
     }
 
     pub fn insert(&self, page: Page<Size2MiB>, frame: PhysFrame<Size2MiB>, count: usize) -> usize {
-        #[cfg(debug_assertions)]
-        let _g = self.lock.lock().unwrap();
+        #[cfg(feature = "page_map_debug")]
+            let mut lock = self.lock.lock().unwrap();
+        #[cfg(feature = "page_map_debug")]{
+            assert!(lock.insert(page, (frame, count as u64)).is_none());
+        }
         const_assert!(COUNT_SHIFT + COUNT_BITS == 64);
         let page = page - self.base_page;
         let frame = frame.start_address().as_u64() >> 21;
@@ -101,12 +117,20 @@ impl PageMap {
     }
 
     pub fn increment_at(&self, index: usize, page: Page<Size2MiB>) {
-        #[cfg(debug_assertions)]
-        let _g = self.lock.lock().unwrap();
-
         const_assert!(PAGE_SHIFT == 0);
+        #[cfg(feature = "page_map_debug")]
+            let mut lock = self.lock.lock().unwrap();
+        #[cfg(feature = "page_map_debug")]
+            let old_debug_count = {
+            let x = &mut lock.get_mut(&page).unwrap().1;
+            *x += 1;
+            *x - 1
+        };
         let old = self.slots[index].fetch_add(1 << COUNT_SHIFT, Relaxed);
-        debug_assert!(page - self.base_page == old & mask(PAGE_BITS));
+        #[cfg(feature = "page_map_debug")]{
+            assert_eq!(old_debug_count, old >> crate::page_map::COUNT_SHIFT);
+            assert_eq!(page - self.base_page, old & crate::page_map::mask(crate::page_map::PAGE_BITS));
+        }
     }
 
     fn target_slot(&self, page: u64) -> usize {
