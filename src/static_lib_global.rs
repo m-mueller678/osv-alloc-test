a61f2f4f74a91e050431291702227d5e1e06b179
m-mueller678 <marcus.mueller.678@gmail.com>
2025-05-02 15:23:12 +0200

    remove zeroing from global_virtual_alloc_alloc

--- src/static_lib_global.rs
+++ src/static_lib_global.rs
@@ -43,18 +43,12 @@ pub unsafe extern "C" fn global_virtual_alloc_init(physical_size: u64, virtual_s
 #[no_mangle]
 pub unsafe extern "C" fn global_virtual_alloc_alloc(size: u64, align: u64) -> *mut libc::c_void {
     catch(|| {
-        // eprintln!("alloc {size}");
         let r = LOCAL.with(|l| {
             l.borrow_mut().alloc(Layout::from_size_align_unchecked(
                 size as usize,
                 align as usize,
             )) as *mut libc::c_void
         });
-        // eprintln!("done alloc {size}");
-        for i in 0..size {
-            (r as *mut u8).add(i as usize).write(0);
-        }
-        // eprintln!("done zero {size}");
         r
     })
 }
@@ -62,14 +56,12 @@ pub unsafe extern "C" fn global_virtual_alloc_alloc(size: u64, align: u64) -> *m
 #[no_mangle]
 pub unsafe extern "C" fn global_virtual_alloc_free(size: u64, align: u64, ptr: *mut libc::c_void) {
     catch(|| {
-        // eprintln!("dealloc {size}");
         LOCAL.with(|l| {
             l.borrow_mut().dealloc(
                 ptr as *mut u8,
                 Layout::from_size_align_unchecked(size as usize, align as usize),
             )
         });
-        // eprintln!("done dealloc {size}");
     })
 }
 
